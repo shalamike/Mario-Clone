686a28d26d681292d98494f63043dbe65066f5eb
shalamike <mshala02@dcs.bbk.ac.uk>
2020-07-25 03:05:50 +0100

    got the bad logic image to pop up throught he playscreen

--- core/src/com/mygdx/game/screens/PlayScreen.java
+++ core/src/com/mygdx/game/screens/PlayScreen.java
@@ -2,6 +2,7 @@ package com.mygdx.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.mygdx.game.MyGdxGame;
 
@@ -21,7 +22,11 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(texture, 0,0);
+        game.batch.end();
     }
 
     @Override
