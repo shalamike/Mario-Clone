a6acef6c2f59f4e21b4b4545f8b73cfd26f27fd0
shalamike <mshala02@dcs.bbk.ac.uk>
2020-07-27 00:00:50 +0100

    completed the inithial hud setup

--- core/src/com/mygdx/game/scenes/Hud.java
+++ core/src/com/mygdx/game/scenes/Hud.java
@@ -1,14 +1,18 @@
 package com.mygdx.game.scenes;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.MyGdxGame;
 
-import java.awt.Label;
+
 
 public class Hud {
     public Stage stage;
@@ -32,6 +36,36 @@ public class Hud {
         timeCount = 0;
         score = 0;
         viewport = new FitViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_height, new OrthographicCamera());
+        // a stage is kind of like an empty box
         stage = new Stage(viewport, sb);
+        // therefore what we need is a table to organise everything inside our empty box
+        Table table = new Table();
+        //by efault the table will be in the centre of our stage
+        table.top();
+        // now the table is on top of the stage
+        table.setFillParent(true);
+        // now the table is the size of the stage
+
+        //time to create the labels
+        countDownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.CORAL));
+        scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.CORAL));
+        timeLabel = new Label("Time ", new Label.LabelStyle(new BitmapFont(), Color.CORAL));
+        levelLabel = new Label("1-1", new Label.LabelStyle(new BitmapFont(), Color.CORAL));
+        worldLabel = new Label("WORLD", new Label.LabelStyle(new BitmapFont(), Color.CORAL));
+        marioLabel = new Label("MARIO", new Label.LabelStyle(new BitmapFont(), Color.CORAL));
+
+        // now time to add the labels to our table
+        // expandX expands the label the entire width of our screen. if multiple items use expandX, then the screen will share those equally by default
+        // pad Top will set the spacing between the top of the screen and our label by 10px in this case
+        table.add(marioLabel).expandX().padTop(10);
+        table.add(worldLabel).expandX().padTop(10);
+        table.add(timeLabel).expandX().padTop(10);
+        table.row();
+        table.add(scoreLabel).expandX();
+        table.add(levelLabel).expandX();
+        table.add(countDownLabel).expandX();
+
+        stage.addActor(table);
+
     }
 }
--- core/src/com/mygdx/game/screens/PlayScreen.java
+++ core/src/com/mygdx/game/screens/PlayScreen.java
@@ -10,25 +10,31 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.scenes.Hud;
 
 public class PlayScreen implements Screen {
     private MyGdxGame game;
-    Texture texture;
-
+    //Texture texture;
     private OrthographicCamera gameCam;
     // creating a viewport to help adapt to multiple screen sizes
-    private Viewport viewport;
+    private Viewport gamePort;
+    private Hud hud;
+
+
     public PlayScreen(MyGdxGame game){
         this.game = game;
-        texture = new Texture("badlogic.jpg");
+        //texture = new Texture("badlogic.jpg");
         //initiallising a new camera for that will follow us along the world called gameCam
         gameCam = new OrthographicCamera();
         //creating a fit viewport that would maintain the aspect ratio of the world regardless of screen size
-        viewport = new FitViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_height, gameCam);
+        gamePort = new FitViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_height, gameCam);
         // alternate viewports
         /*
         * StretchViewport would stretch out or compress the image to fit the different size screens
         * screen viewports will allow the user to see more or less of the game world depending on the size*/
+        hud = new Hud(game.batch);
+
+
     }
 
     @Override
@@ -40,16 +46,15 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.setProjectionMatrix(gameCam.combined);// this line declares what we are allowed to see in this case its what the camera sees.
-        game.batch.begin();
-        game.batch.draw(texture, 0,0);
-        game.batch.end();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);// this line declares what we are allowed to see in this case its what the camera sees.
+        hud.stage.draw();
     }
 
     //resize method will resize the image depending on the size of the screen
     @Override
     public void resize(int width, int height) {
-        viewport.update(width,height);
+        gamePort.update(width,height);
     }
 
     @Override
