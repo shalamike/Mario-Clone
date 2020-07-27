e0e44d41f61b4ecbd5d2f1c421f97300ecdd07c0
shalamike <mshala02@dcs.bbk.ac.uk>
2020-07-27 04:58:35 +0100

    added tileset from brent aurellis tutorial and started implementing some basic code

--- core/src/com/mygdx/game/screens/PlayScreen.java
+++ core/src/com/mygdx/game/screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -16,23 +19,30 @@ public class PlayScreen implements Screen {
     private MyGdxGame game;
     //Texture texture;
     private OrthographicCamera gameCam;
-    // creating a viewport to help adapt to multiple screen sizes
-    private Viewport gamePort;
+    private Viewport gamePort; // creating a viewport to help adapt to multiple screen sizes
     private Hud hud;
+
+    private TmxMapLoader mapLoader; //this is what actually loads in our game map
+    private TiledMap map; // reference to the map itself
+    private OrthogonalTiledMapRenderer renerer; // this is used to render the map onto our camera
 
 
     public PlayScreen(MyGdxGame game){
         this.game = game;
         //texture = new Texture("badlogic.jpg");
-        //initiallising a new camera for that will follow us along the world called gameCam
-        gameCam = new OrthographicCamera();
-        //creating a fit viewport that would maintain the aspect ratio of the world regardless of screen size
-        gamePort = new FitViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_height, gameCam);
+        gameCam = new OrthographicCamera();//initiallising a new camera for that will follow us along the world called gameCam
+        gamePort = new FitViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_height, gameCam); //creating a fit viewport that would maintain the aspect ratio of the world regardless of screen size
         // alternate viewports
         /*
         * StretchViewport would stretch out or compress the image to fit the different size screens
-        * screen viewports will allow the user to see more or less of the game world depending on the size*/
-        hud = new Hud(game.batch);
+        * screen viewports will allow the user to see more or less of the game world depending on the size
+        * */
+        hud = new Hud(game.batch); // initialising the hud taking the game.batch as the argument
+
+        mapLoader = new TmxMapLoader();// initiallising the map loader
+        map = mapLoader.load("level1.tmx"); // initialising the map using the map loader to load our tiled level
+        renerer = new OrthogonalTiledMapRenderer(map); //rendering the map after it has been loaded
+        gameCam.position.set(gamePort.getScreenWidth()/2 , gamePort.getScreenHeight()/2, 0);
 
 
     }
@@ -44,16 +54,16 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,0,0,1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(1,0,0,1); //this line clears the screen to a basic red colour whenever clear is glClear is called
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // this line clears the screan to the colur abouve;
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);// this line declares what we are allowed to see in this case its what the camera sees.
-        hud.stage.draw();
+        hud.stage.draw();//this line will display the given parameters defined above
     }
 
-    //resize method will resize the image depending on the size of the screen
+
     @Override
-    public void resize(int width, int height) {
+    public void resize(int width, int height) { //resize method will resize the image depending on the size of the screen
         gamePort.update(width,height);
     }
 
